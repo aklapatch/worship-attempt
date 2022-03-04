b9ad1fd062c5722ed8307c8c2cabd6bd5ddc279c
Aaron Klapatch <Aaron>
2022-03-04 12:47:14 -0500

    Got a fullscreen to show from clicking a button.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-use fltk::{app, prelude::*, window::Window};
+use fltk::{app, button::Button, frame::Frame, prelude::*, window::Window};
 
 fn main() {
-    let app = app::App::default();
-    let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
+    let app = app::App::default().with_scheme(app::Scheme::Gleam);
+    let mut wind = Window::new(100, 100, 400, 300, "Editor window");
+
+    let mut frame = Frame::new(0, 0, 400, 200, "");
+    let mut but = Button::new(160, 210, 80, 40, "open window");
+    wind.make_resizable(true);
     wind.end();
     wind.show();
+
+    let mut wind1 = Window::default()
+        .with_label("test2");
+    wind1.fullscreen(true);
+    wind1.clear_modal_states();
+    wind1.end();
+    wind1.hide();
+
+    but.set_callback(move |_| wind1.show());
+
     app.run().unwrap();
 }
 
